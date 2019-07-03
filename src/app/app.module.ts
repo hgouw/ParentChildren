4e9d9003ec612487d36273bcda1ced99afa4a130
Herman Gouw <hermangouw@gmail.com>
2019-07-03 21:24:28 +1000

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildrenComponent } from './children/children.component';
+import { FirstChildComponent } from './first-child/first-child.component';
+import { SecondChildComponent } from './second-child/second-child.component';
+import { ThirdChildComponent } from './third-child/third-child.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ParentComponent,
+    ChildrenComponent,
+    FirstChildComponent,
+    SecondChildComponent,
+    ThirdChildComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/third-child/third-child.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-third-child',
+  templateUrl: './third-child.component.html',
+  styleUrls: ['./third-child.component.css']
+})
+export class ThirdChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
